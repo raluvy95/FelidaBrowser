b8d368eea089c8d7128b1a5572cca10a4b2206af
raluvy95 <ralucadoge069@gmail.com>
2023-05-26 18:44:00 +0300

    improvement in run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ $(whoami) = "root" ]; then
+if [ "$(whoami)" = "root" ]; then
     echo "Warning: running as root is not recommend."
 fi
 
 if [ $# = 0 ]; then
     node ./node_modules/electron/cli.js .
-elif [ $1 = "--help" ]; then
-    VERSION=`(cat package.json | jq '.version' | sed -e 's/^"//' -e 's/"$//') || unknown`
+elif [ "$1" = "--help" ]; then
+    VERSION=$(jq '.version' < package.json | sed -e 's/^"//' -e 's/"$//') || unknown
     echo "Help command"
     echo "--help = Shows this message"
     echo "--log  = Start in debug mode"
     echo "FelidaBrowser v$VERSION"
 else
-    node ./node_modules/electron/cli.js . $*
+    node ./node_modules/electron/cli.js . "$*"
 fi
